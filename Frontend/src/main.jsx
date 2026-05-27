7239a4464c9c53d792745f122c6a191f67a90f2f
Shreyanshp0 <145826998+Shreyanshp0@users.noreply.github.com>
2026-05-27 15:30:18 +0530

    Fixed the monaco CDN issue

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './config/monaco'
 import App from './App.jsx'
 import AppErrorBoundary from './components/common/AppErrorBoundary'
 
